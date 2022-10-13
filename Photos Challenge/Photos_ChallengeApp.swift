11e7975d47ad7caa2f17636f194b416b32791c3a
sdGuille <guillermoruizbaires@gmail.com>
2022-10-13 10:15:58 -0600

    Initial Commit

---
+++ Photos Challenge/Photos_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photos_ChallengeApp.swift
+//  Photos Challenge
+//
+//  Created by Guillermo Ruiz on 13/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Photos_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
